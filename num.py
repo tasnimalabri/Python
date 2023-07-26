3992867b34d4188b50ac42da2d949cde21152929
tasnimalabri <tasnimsaif2000@gmail.com>
2023-07-26 11:40:46 +0400

    num

---
+++ num.py
@@ -0,0 +1,15 @@
+num1= float(input("Enter number1: "))
+num2= float(input("Enter number2: "))
+opr = input("Ener operator: ")
+
+if (opr == '+'):
+     print(num1 + num2)
+elif (opr == '-'):
+     print(num1 - num2)
+elif (opr == '*'):
+     print(num1 * num2)
+elif (opr == '/'):
+     print(num1 / num2)
+else:
+    print("Invlid Operator")
+ 
